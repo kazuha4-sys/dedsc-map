fcccc0c69d39cc69546b018c91cd77b591bf5b62
kazuha4-sys <cleuzadasilvadossantos@gmail.com>
2026-03-20 21:12:43 -0300

    service workfloe

--- sw.js
+++ sw.js
@@ -1,7 +1,42 @@
-self.addEventListener("install", e=> {
-    console.log("Service Worker instalado");
+const CACHE_NAME = "dedsec-cache-v1";
+
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/icon512_maskable.png",
+  "/icon512_rounded.png"
+];
+
+// INSTALAÇÃO
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
-self.addEventListener("fetch", e => {
-    // só intercepta, básico   
+// ATIVAÇÃO
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+// FETCH (INTERCEPTA REQUISIÇÕES)
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
